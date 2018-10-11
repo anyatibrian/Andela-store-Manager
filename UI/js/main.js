ab405d626020405ec258e2e5ed97e9edff3ea647
anyatibrian <anyatibrian@gmail.com>
2018-10-11 14:20:30 +0300

    [finishes #161045572] finished the feature that enables attends view the sales record

---
+++ UI/js/main.js
@@ -0,0 +1,33 @@
+
+
+var add_pdt_modal = document.getElementById('add-product-modal');
+var edit_add_product_modal =document.getElementById('edit-add-product-modal');
+var edit_attend_modal = document.getElementById('edit-attend-modal');
+var add_attend_modal  = document.getElementById('add-attend-modal');
+/* displaying the edit food modal*/
+function displayAddPdtModal(){
+    add_pdt_modal.style.display = 'block';
+
+}
+/* function display edit product modal boxes */
+function displayEditPdtModals() {
+    edit_add_product_modal.style.display ='block';
+}
+/* display the add attend model*/
+function displayEditAttendantModals() {
+    edit_attend_modal.style.display ='block';
+}
+/* display the add attend model*/
+function displayAddAttendantModals() {
+    add_attend_modal.style.display ='block';
+}
+/* the close modal function */
+function closeModal(){
+    add_pdt_modal.style.display = 'none';
+    edit_add_product_modal.style.display = 'none';
+
+}
+function close_attendant_model() {
+     edit_attend_modal.style.display ='none';
+     add_attend_modal.style.display ='none';
+}
