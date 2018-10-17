44eff37e287d80a54506e9198e31fdb590539e02
anyatibrian <anyatibrian@gmail.com>
2018-10-17 11:05:45 +0300

    written test for get all product endpoints [finishes #161228244]

--- test/test_get_all_products_endpoints.py
+++ test/test_get_all_products_endpoints.py
@@ -3,10 +3,7 @@ from flask import json
 
 
 # testing the post product endpoint
-def test_get_product_endpoints(client, data):
-    """checking wether some data was posted"""
-    response = client.post('api/v1/products', data=json.dumps(data))
-    assert response.status_code == 201
+def test_get_product_endpoints(client):
     """getting all the products that were posted"""
     response = client.get('api/v1/products')
     assert response.status_code == 200
