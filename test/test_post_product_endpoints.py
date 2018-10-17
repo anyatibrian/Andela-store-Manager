7f0e0746e8f2c5300fbda7ece67d771c1c19af3b
anyatibrian <anyatibrian@gmail.com>
2018-10-17 10:21:44 +0300

    written test for the post producte end point [finishes #161227843]

--- test/test_post_product_endpoints.py
+++ test/test_post_product_endpoints.py
@@ -2,6 +2,7 @@ from test import client, data
 from flask import json
 
 
+# testing the post product endpoint
 def test_post_product_endpoints(client, data):
     """testing the post product endpoints"""
     response = client.post('api/v1/products', data=json.dumps(data))
