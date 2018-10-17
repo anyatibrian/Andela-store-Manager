558b6460476bd5aa7e78723de59f0c961fae7331
anyatibrian <anyatibrian@gmail.com>
2018-10-17 17:10:20 +0300

    writen taste case for get all product enpoints [finishes #161254534]

--- Api/apiv1/__init__.py
+++ Api/apiv1/__init__.py
@@ -2,4 +2,4 @@ from flask import Blueprint
 
 api_blueprint = Blueprint('api_blueprint', __name__)
 
-from . import get_single_product_endpoints
+from . import get_all_sales_record_endpoints
--- test/test_get_all_sales_record_endpoints.py
+++ test/test_get_all_sales_record_endpoints.py
@@ -3,7 +3,7 @@ from flask import json
 
 
 # testing the post product endpoint
-def test_get_single_product_endpoint(client):
+def test_get_all_sales_records_endpoints(client):
     """testing the post product endpoints"""
-    response = client.get('api/v1/products/{}'.format(1))
+    response = client.get('api/v1/sales')
     assert response.status_code == 200
