4c3f1ac261bb73e7b6c861a6e8d2588fed9b5456
anyatibrian <anyatibrian@gmail.com>
2018-10-17 16:33:55 +0300

    refacture the test  [finishes #161283751]

--- test/test_get_single_records_endpoints.py
+++ test/test_get_single_records_endpoints.py
@@ -4,6 +4,6 @@ from flask import json
 
 # testing the post product endpoint
 def test_get_single_records_endpoint(client):
-    """testing the post product endpoints"""
+    """testing the get single records endpoints"""
     response = client.get('api/v1/sales/{}'.format(1))
     assert response.status_code == 200
