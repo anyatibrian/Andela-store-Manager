a276413115b496954e9d701abdacbaad80d223f0
anyatibrian <anyatibrian@gmail.com>
2018-10-18 15:57:01 +0300

    created get single record routes[finisheds #161283751]

--- Api/apiv1/__init__.py
+++ Api/apiv1/__init__.py
@@ -1,4 +1,4 @@
 from flask import Blueprint
 
 api_blueprint = Blueprint('api_blueprint', __name__)
-from . import get_single_product, get_all_products, posts_product, posts_sales_record
+from . import get_single_product, get_all_products, posts_product, posts_sales_record, get_single_sale
