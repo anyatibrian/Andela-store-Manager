84535e8c6e371c56e9e6c3b9fd9f5e4ef21e6249
anyatibrian <anyatibrian@gmail.com>
2018-10-18 13:44:00 +0300

    finished the get all product feature [finishes#161228244]

---
+++ Api/apiv1/get_all_products.py
@@ -0,0 +1,12 @@
+from Api.modules.products import product_list
+from flask import jsonify, make_response
+from ..apiv1 import api_blueprint
+
+
+@api_blueprint.route('/products', methods=['GET'])
+def get_products():
+    """endpoints to fetch all the products"""
+    if len(product_list) != 0:
+        return make_response(jsonify({'products': product_list}), 200)
+    else:
+        return make_response(jsonify({'message': "you don't have any product yet "}), 200)
