c938c8f8a47f1eab9de382930b59dffce4c63da0
anyatibrian <anyatibrian@gmail.com>
2018-10-18 14:48:05 +0300

    finished working on the get single product feature [finishes #161229417]

--- Api/apiv1/__init__.py
+++ Api/apiv1/__init__.py
@@ -1,4 +1,4 @@
 from flask import Blueprint
 
 api_blueprint = Blueprint('api_blueprint', __name__)
-from . import posts_product, get_all_products
+from . import posts_product, get_all_products, get_single_product
