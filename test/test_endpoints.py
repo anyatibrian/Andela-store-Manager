7295670b8e3c5cc039082fcbd5063f6e10cebff9
anyatibrian <anyatibrian@gmail.com>
2018-10-19 20:22:37 +0300

    mades some changes to the files

--- test/test_endpoints.py
+++ test/test_endpoints.py
@@ -20,8 +20,6 @@ def data():
     }
     return data
 
-
-<<<<<<< HEAD
 @pytest.fixture()
 def sales_data():
     data = {
@@ -31,33 +29,12 @@ def sales_data():
         "unit_price": "400"
     }
     return data
-def test_checking_for_empty_field(client):
-    response = client.post('api/v1/products', data=json.dumps({
-        "product_name": "",
-        "description": "",
-        "quantity": "900",
-        "price": "2000"
-    }
-    ))
-    assert b"some fields are empty" in response.data
-
-
-def test_post_data_type(client):
-    response = client.post('api/v1/products', data=json.dumps({
-        "product_name": "touch",
-        "description": "people like it",
-        "quantity": "popo",
-        "price": "popolipop"
-    }
-    ))
-    assert b"invalid types" in response.data
-
 
 # testing the post product endpoint
 def test_post_product_endpoints(client, data):
     """testing the post product endpoints"""
     response = client.post('api/v1/products', data=json.dumps(data))
-    assert response.status_code == 200
+    assert response.status_code == 201
 
 
 # testing the post product endpoint
