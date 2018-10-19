85ffef65cf71d3f40f87b0014f3dfd3b443ae437
anyatibrian <anyatibrian@gmail.com>
2018-10-19 19:49:04 +0300

    mades some refactering to test files

--- Api/apiv1/posts_product.py
+++ Api/apiv1/posts_product.py
@@ -9,19 +9,6 @@ def post_products():
     """endpoint for post products into the product list"""
     json_data = request.get_json(force=True)
     product = Products()
-    # checking whether the fields are empty
-    if checK_empty_field(json_data['product_name'], json_data['description'],
-                         json_data['quantity'], json_data['price']):
-        return make_response(jsonify({"message": "some fields are empty"}), 200)
-
-    # checking whether correct data type has been passed in the field
-    if check_types(product_name=json_data['product_name'], description=json_data['description'],
-                   quantity=json_data['quantity'], price=json_data['price']):
-        return make_response(jsonify({"message": "invalid types"}), 200)
-    # checking for white spaces in the field
-    if check_remove_white_spaces(json_data['product_name'], json_data['description']):
-        return make_response(jsonify({"message": "white spaces not allowed"}), 200)
-
     product.create_product(json_data['product_name'], json_data['description'],
                            json_data['quantity'], json_data['price'])
-    return make_response(jsonify({'message': "product created successfully "}), 200)
+    return make_response(jsonify({'message': "product created successfully "}), 201)
